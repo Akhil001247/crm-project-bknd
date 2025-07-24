4b08117ae768a9aa9de1bf83868100d0756b1285
Akhil001247 <2006akhilraju2@gmail.com>
2025-07-24 22:28:28 +0530

    modified server.js added

--- server.js
+++ server.js
@@ -8,23 +8,31 @@ const customerRoutes = require('./routes/customerRoutes');
 
 const app = express();
 
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://crm-project-frnd.vercel.app'],
-  credentials: true
+  origin: ['http://localhost:5173', 'https://crm-project-frnd-w52h.vercel.app'],
+  credentials: true,
 }));
+
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB Connected'))
+.catch((err) => console.error(' MongoDB connection error:', err));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/customers', customerRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server started on port ${PORT}`));
